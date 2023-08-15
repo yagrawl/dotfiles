f10ab27fe7653f5595170cefbe20c6909b061c53
Yash Agrawal <yagrawl2@gmail.com>
2023-08-15 01:17:44 -0400

    modify symlink setup for zsh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
+set -e
 
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+# ANSI escape codes for colors
+BLUE="\033[38;5;27m"
+PURPLE="\033[38;5;177m"
+RESET="\033[0m"
+
+create_symlink() {
+  local source="$1"
+  local target="$2"
+
+  if [ -e "$target" ]; then
+    echo -e "File ${PURPLE}$target${RESET} already exists, skipping symlink creation."
+  else
+    ln -s "$source" "$target"
+    echo -e "Symlink ${BLUE}$source${RESET} -> ${PURPLE}$target${RESET} created."
+  fi
+}
+
+create_symlink ~/dotfiles/.zshrc ~/.zshrc
+create_symlink ~/dotfiles/.p10k.zsh ~/.p10k.zsh
+create_symlink ~/dotfiles/.gitconfig ~/.gitconfig
