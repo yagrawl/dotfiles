d8ce09e1427b3e4f4d8bdde6e28030e6a1479588
Yash Agrawal <yagrawl2@gmail.com>
2023-08-20 23:03:20 -0400

    fix/add LSPs - solargraph, eslint, json, lua

--- nvim/lua/core/plugin_config/lsp_config.lua
+++ nvim/lua/core/plugin_config/lsp_config.lua
@@ -1,9 +1,57 @@
 require("mason").setup()
-require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls" }
+require("mason-lspconfig").setup()
+
+require("lspsaga").setup({
+  code_action_icon = "💡",
+  symbol_in_winbar = {
+    in_custom = false,
+    enable = true,
+    separator = ' › ',
+    show_file = true,
+    file_formatter = ""
+  },
 })
 
-vim.keymap.set('n', '\\gd', vim.lsp.buf.definition, {})
-vim.keymap.set('n', '\\gi', vim.lsp.buf.implementation, {})
+local lspconfig = require('lspconfig')
 
-require("lspconfig").lua_ls.setup {}
+-- Define an on_attach function to set up keybindings
+local on_attach = function(_, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local opts = { noremap=true, silent=true }
+
+  -- Define keybindings for LSP functions
+  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+end
+
+-- Solargraph for Ruby
+lspconfig.solargraph.setup {
+  filetypes = { 'ruby' },
+  settings = {
+    solargraph = {
+      diagnostics = true
+    }
+  },
+  on_attach = on_attach
+}
+
+-- ESLint for JavaScript
+lspconfig.eslint.setup {
+  filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+  on_attach = on_attach
+}
+
+-- Lua language server
+lspconfig.lua_ls.setup {
+  filetypes = { 'lua' },
+  on_attach = on_attach
+}
+
+-- JSON language server
+lspconfig.jsonls.setup {
+  filetypes = { 'json' },
+  on_attach = on_attach
+}
+
