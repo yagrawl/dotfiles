0bb59d30cf7e767cd70eb39666fad9ff26e7aa8a
Yash Agrawal <yagrawl2@gmail.com>
2023-08-20 22:00:16 -0400

    move option config from keymaps

---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,28 @@
+local opt = vim.opt
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+opt.backspace = '2'
+opt.showcmd = true
+opt.laststatus = 2
+opt.autowrite = true
+opt.cursorline = true
+opt.autoread = true
+
+-- use spaces for tabs
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.shiftround = true
+opt.expandtab = true
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+vim.cmd [[ set noswapfile ]]
+
+--Line numbers
+vim.wo.number = true
